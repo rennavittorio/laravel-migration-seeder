89366862ee8372efcd670c3eefcc5ab279ad9c24
rennavittorio <renna.vittorio@gmail.com>
2023-04-12 15:05:43 +0200

    add: add migration table and columns

---
+++ database/migrations/2023_04_12_124515_create_trains_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('company', 100);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->tinyInteger('train_code')->unsigned();
+            $table->tinyInteger('carriages')->unsigned();
+            $table->tinyInteger('isOnTime')->unsigned()->default(1);
+            $table->tinyInteger('isCancelled')->unsigned()->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
